edbc7dd8f731fef80b54791211f94a4fca4136aa
丁文超 <13963189468@163.com>
2022-10-12 22:08:53 +0800

    update

--- mmdetection/inference.py
+++ mmdetection/inference.py
@@ -3,36 +3,39 @@
 from re import L
 from mmdet.apis import init_detector
 from mmdet.apis import inference_detector
-# from mmdet.apis import show_result
 import numpy as np
-
-
-
-# config_file = 'configs/swin/cascade_rcnn_swin.py'
-# checkpoint_file = 'work_dirs/cascade_rcnn_swin/epoch_22.pth'
-# model = init_detector(config_file, checkpoint_file, device='cuda:0')
-# img = 'datasets/road2020/test1/test_images/Czech_002798.jpg'
-# result = inference_detector(model, img)
-# # import pdb;pdb.set_trace()
-# # import pdb;pdb.set_trace()
-# model.show_result(img, result, out_file='test_640flip_Czech_002798.jpg', score_thr=0.1)
-# 1599
-#test00 C2798
-
+from mmcv import Config, DictAction
 import glob
 import tqdm
 import os
-# config_file = 'configs/swin/faster_swin_l.py'
-# checkpoint_file = 'swin_epoch_36.pth'
-config_file = 'configs/dyhead/atss_swin-l-p4-w12_fpn_dyhead_mstrain_2x_coco.py'
-checkpoint_file = 'dyhead_epoch_36.pth'
-# config_file = 'configs/swin/faster_swin_l12_deform.py'
-# checkpoint_file = 'swin12w_deform_epoch_36.pth'
-# config_file = 'configs/detectors/detectors_cascade_rcnn_r50_1x_coco.py'
-# checkpoint_file = 'detectors_epoch_20.pth'
-path = 'datasets/RDD2022/test1_images/'
-# path = 'datasets/RDD2022/Japan/train/images/'
-model = init_detector(config_file, checkpoint_file, device='cuda:0')
+import argparse
+
+
+parser = argparse.ArgumentParser(description='...')
+parser.add_argument('config', help='test config file path')
+parser.add_argument('checkpoint', help='checkpoint file')
+parser.add_argument('source', help='test images path')
+parser.add_argument('imgsz', help='test imgsz')
+parser.add_argument('conf_thres', help='test imgsz')
+parser.add_argument('iou_thres', help='test imgsz')
+parser.add_argument('filename', help='outputfilename')
+args = parser.parse_args()
+
+
+config_file = args.config
+checkpoint_file = args.checkpoint
+path = args.source
+imgsz = args.imgsz
+conf_thres = args.conf_thres
+iou_thres = args.iou_thres
+filename = args.filename
+
+cfg = Config.fromfile(config_file)
+cfg['test_pipeline'][1]['img_scale'] = (imgsz, imgsz)
+cfg.model.test_cfg.rcnn.score_thr = float(conf_thres)
+cfg.model.test_cfg.rcnn.nms.iou_threshold = float(iou_thres)
+
+model = init_detector(cfg, checkpoint_file, device='cuda:0')
 for image_name in tqdm.tqdm(os.listdir(path)):
     result = inference_detector(model, path+image_name)
     bbox_result = result
@@ -54,5 +57,5 @@ for image_name in tqdm.tqdm(os.listdir(path)):
                     + str(ymin) + ' ' + str(xmax) + ' ' + str(ymax) + ' '
     bbox_str += '\n'
     # with open('results_mmdet/detectors_22_1_e20_88_iou0.9999_conf0.50.txt', 'a') as w:
-    with open('results_mmdet/dyhead_e36_all_72_conf0.05_iou0.6.txt', 'a') as w:
+    with open('results_mmdet/' + filename, 'a') as w:
         w.write(image_name + ',' + bbox_str)
